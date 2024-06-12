63a6baa0fffcd58d217b9db9fe3b84a06ddc6b17
Nemil21 <nemilshah212005@gmail.com>
2024-06-12 19:05:30 +0530

    modified data

--- data/index.ts
+++ data/index.ts
@@ -72,68 +72,68 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "https://github.com/adrianhajdin?tab=repositories",
+      title: "YelpCamp",
+      des: "Jump right in to explore our many campgrounds.Feel free to share some of your own and coment on others!",
+      img: "/YelpCampHomePage.png",
+      iconLists: ["/cloud.svg", "/bootstrap-4.svg", "/js.svg", "/node.svg", "/mongo.svg"],
+      link: "https://yelpcamp-05qe.onrender.com/",
     },
     {
       id: 2,
-      title: "Yoom - Video Conferencing App",
-      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-      img: "/p2.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/zoom-clone",
+      title: "Dhruv Developers",
+      des: "Own Your Dream Home with Dhruv Developers today!",
+      img: "/dhruvdevhome.jpeg",
+      iconLists: ["/wp.svg", "/xampp.svg"],
+      link: "https://dhruvdevelopers.com/",
     },
-    {
-      id: 3,
-      title: "AI Image SaaS - Canva Application",
-      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/ai_saas_app",
-    },
-    {
-      id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
-    },
+    // {
+    //   id: 3,
+    //   title: "AI Image SaaS - Canva Application",
+    //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    //   img: "/p3.svg",
+    //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    //   link: "https://github.com/adrianhajdin/ai_saas_app",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Animated Apple Iphone 3D Website",
+    //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    //   img: "/p4.svg",
+    //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    //   link: "https://github.com/adrianhajdin/iphone",
+    // },
   ];
   
   export const testimonials = [
     {
       quote:
-        "Collaborating with Nemil was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nemil's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Collaborating with Nemil was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nemil's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Nemil is the ideal partner.",
+      name: "Dhruvin Shah",
+      title: "Owner of Dhruv Developers",
     },
     {
       quote:
-        "Collaborating with Nemil was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nemil's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Collaborating with Nemil was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nemil's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Nemil is the ideal partner.",
+      name: "Dhruvin Shah",
+      title: "Owner of Dhruv Developers",
     },
     {
       quote:
-        "Collaborating with Nemil was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nemil's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Collaborating with Nemil was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nemil's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Nemil is the ideal partner.",
+      name: "Dhruvin Shah",
+      title: "Owner of Dhruv Developers",
     },
     {
       quote:
-        "Collaborating with Nemil was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nemil's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Collaborating with Nemil was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nemil's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Nemil is the ideal partner.",
+      name: "Dhruvin Shah",
+      title: "Owner of Dhruv Developers",
     },
     {
       quote:
-        "Collaborating with Nemil was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nemil's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Collaborating with Nemil was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nemil's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Nemil is the ideal partner.",
+      name: "Dhruvin Shah",
+      title: "Owner of Dhruv Developers",
     },
   ];
   
@@ -173,29 +173,29 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Full Stock Project",
+      desc: "Built and Deployed an app using MERN Stack",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "Mobile App Dev - Flutter",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using Flutter.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      title: "Freelance Web Dev Project",
+      desc: "Led the dev of a web app for a client, from initial concept to deployment.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      title: "Team Lead for SIH",
+      desc: "Led a team to 6 to develop a website that helps hospitals optimize their workflow. Qualified for Round 2",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
